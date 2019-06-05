fa5d8de93698fe0bf9ba7a59b2facfa26e4f1e75
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 18:36:22 +0000

    Automatically backed up by Learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,17 @@
 // Code ControlledInput Component Here
+
+class Form extends Component {
+  state = {
+    firstName: "John",
+    lastName: "Henry"
+  }
+ 
+  render() {
+    return (
+      <form>
+        <input type="text" name="firstName" value={this.state.firstName} />
+        <input type="text" name="lastName" value={this.state.lastName} />
+      </form>
+    )
+  }
+}
