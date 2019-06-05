4e3cbc420eb1d709b27d9f8a9092f0c9e26e2cf1
Yoonmi Kim <yoonmi.kim@rutgers.edu>
2019-06-05 18:46:54 +0000

    Automatically backed up by Learn

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,42 +1,28 @@
 // Code ControlledInput Component Here
-
-class Form extends Component {
+import React from 'react';
+ 
+class ControlledInput extends React.Component {
   state = {
-    firstName: "John",
-    lastName: "Henry",
     value: '',
   }
-  
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
-  }
  
-  handleLastNameChange = event => {
+  handleChange = event => {
     this.setState({
-      lastName: event.target.value
-    })
+      value: event.target.value,
+    });
   }
-  
-  handleSubmit = event => {
-  event.preventDefault()
-  const firstName = event.target.children[0].value
-  const lastName = event.target.children[1].value
-  this.sendFormDataSomewhere({ firstName, lastName })
-}
  
   render() {
     return (
-      <form>
-        <input type="text" id="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        
-        <input type="text" id="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-        
-        <form onSubmit={ event => this.handleSubmit(event) }>
+      <form onSubmit={event => this.handleSubmit(event)}>
+        <input
+          type="text"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
       </form>
-    )
+    );
   }
 }
-
-export default ControlledInput
+ 
+export default ControlledInput;
